63876a571a4fa191c5b067d9d982cb71e094d735
Talha <talhaayar53@gmail.com>
2020-01-30 16:24:07 +0300

    Initial Commit

---
+++ TabbarExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabbarExample
+//
+//  Created by TALHA AYAR on 30.01.2020.
+//  Copyright © 2020 Talha Ayar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
